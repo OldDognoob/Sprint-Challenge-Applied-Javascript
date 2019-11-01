d529c2cc25c163b56655eca0c08a77cb997b9ff9
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-11-01 11:38:46 +0200

    tabs complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,3 +19,25 @@
 // Create a card for each of the articles and add the card to the DOM.
 
 
+// Making API request
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+
+
+// .then(response => console.log(response.data))
+.then (response =>{
+
+    const cardsContainer = document.querySelector('.cards-container');
+
+    const articlesObject = response.data.articles;
+
+    const articlesTypes = Object.keys(articlesObject);
+
+    articlesTypes.forEach(article => {
+        for(let i=0; i< article.length; i++){
+            cardsContainer(article[i]);
+        }
+    })
+})
+.catch (error =>{
+  console.log(error);
+});
