59d8ca28863b28d7a722f6ecd5a2753e09517360
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-11-01 10:46:38 +0200

    step1 header component complete

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+// Making API request
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+
+// .then(staff => console.log(staff))
+
+.then(staff => staff.data.topics.forEach(element => new Tab(element)))
+
+function Tab(tabname){
+    const wrapper = document.querySelector('.topics')
+
+    const tab = document.createElement('div')
+    wrapper.appendChild(tab)
+    tab.classList.add('tab')
+    tab.textContent= tabname
+    
+
+    return tab
+}
